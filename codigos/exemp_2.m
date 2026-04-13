7e02886f44142ef0628b635ae376e29f3130fd01
André, O Magnífico <ami.silva@campus.fct.unl.pt>
2026-04-13 12:54:04 +0100

    Update and rename exemps.m to exemp_2.m

--- codigos/exemp_2.m
+++ codigos/exemp_2.m
@@ -1,27 +1,12 @@
-function tabela_final = exemps ()
+function tabela_final = exemp_2 ()
 h=[0.25 0.2 0.1 0.05];
 
-%os exemplos estão abaixo, comentar dependendo do que se deseja
 
-%exemplo 1
-%eu fiz o exemplo 2 primeiro, onde o t_i comum a todos os passos eram o 1º(nao tem utilidade) e o último
-%no exemplo 1 vao aparecer 2 t_i relevantes (o 1 e o 2), as matrizes têm um nº impar de colunas, o 1 é sempre nºcolunas/2 + 0.5
-%deve dar para fazer assim, mas a tabela final vai precisar de alterações...
-
-f=@(t,y) -30*y;
-T=2;
-alfa=1/3;
-y=@(t) (1/3)*e^(-30*t);
-
-
-%exemplo 2, o f não é linear pelo que está a dar Inf em algumas aproximações dos metodos implicitos
-%TO DO: resolver os Inf, talvez não seja possivel, verificar à mão e euler implicito de passo 0.25 - ele estraga logo, deve ser fácil :)
-#{
 f=@(t,y) 5 * e^(5*t) * (y-t)^2 + 1;
 T=1;
 alfa=-1;
 y=@(t) t - e^(-5*t);
-#}
+
 
 N=zeros(1,4);
 % o valor de N depende de h nos exemplos
